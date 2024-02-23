454264a913ff7905091411baabb50be30048e90f
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-02-23 18:16:38 +0800

    Finish basic ugly mine and stake page

--- FrontEnd/src/pages/Mine/index.tsx
+++ FrontEnd/src/pages/Mine/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import ContentWrapper from "@/sections/ContentWrapper";
-import { Tabs, InputNumber, Button } from "antd";
 import type { TabsProps } from "antd";
+import { Tabs, InputNumber, Button } from "antd";
+import InfoCard from "@/components/InfoCard";
 
 function index() {
     const SingleMiner = () => {
@@ -74,6 +74,87 @@ function index() {
         },
     ];
 
+    const infoData = [
+        {
+            key: "1",
+            label: "Summary & Estimated ROI",
+            content: [
+                {
+                    key: "1.1",
+                    label: "Est. TITAN X at End of Miner",
+                    value: "0",
+                    tips: "Est. TITAN X at End of Miner",
+                },
+                {
+                    key: "1.2",
+                    label: "ETH to Start Miner",
+                    value: "3",
+                    tips: "ETH to Start Miner",
+                },
+                {
+                    key: "1.3",
+                    label: "$ Market Value of Miner",
+                    value: "3",
+                    tips: "Market Value of Miner",
+                },
+                {
+                    key: "1.4",
+                    label: "Est. ROI % at End of Miner",
+                    value: "3",
+                    tips: "Est. ROI % at End of Miner",
+                },
+            ],
+        },
+        {
+            key: "2",
+            label: "TITAN X Details",
+            content: [
+                {
+                    key: "2.1",
+                    label: "TITAN X Market Price",
+                    value: "$0.000000744",
+                    tips: "TITAN X Market Price",
+                },
+            ],
+        },
+        {
+            key: "3",
+            label: "TITAN X Miner Details",
+            content: [
+                {
+                    key: "3.1",
+                    label: "Global TRank",
+                    value: "$0.000000744",
+                    tips: "Global TRank",
+                },
+                {
+                    key: "3.2",
+                    label: "Current Titan Per Day of Mining",
+                    value: "$0.000000744",
+                    tips: "Current Titan Per Day of Mining",
+                },
+                {
+                    key: "3.3",
+                    label: "🚀 Early Adoption Amplifier",
+                    value: "$0.000000744",
+                    tips: "🚀 Early Adoption Amplifier",
+                },
+                {
+                    key: "3.4",
+                    label: "🔥 Burn Bonus Amplifier",
+                    value: "$0.000000744",
+                    tips: "🔥 Burn Bonus Amplifier",
+                },
+                {
+                    key: "4.5",
+                    label: "Next Difficulty Increase",
+                    value: "$0.000000744",
+                    tips: "Next Difficulty Increase",
+                },
+            ],
+        },
+    ];
+
     return (
         <div>
             <ContentWrapper title="Mine" subTitle="Create your TITAN X virtual miners">
@@ -81,7 +162,9 @@ function index() {
                     <div className="w-1/2">
                         <Tabs defaultActiveKey="1" items={items} onChange={() => {}} />
                     </div>
-                    <div className="w-1/2 bg-slate-50">right</div>
+                    <div className="w-1/2">
+                        <InfoCard data={infoData} />
+                    </div>
                 </div>
             </ContentWrapper>
         </div>
--- FrontEnd/src/pages/Stake/index.tsx
+++ FrontEnd/src/pages/Stake/index.tsx
@@ -1,7 +1,129 @@
-import React from "react";
+import ContentWrapper from "@/sections/ContentWrapper";
+import { InputNumber, Button } from "antd";
+import InfoCard from "@/components/InfoCard";
 
 function index() {
-    return <div>stake</div>;
+    const SingleMiner = () => {
+        return (
+            <div className="p-6">
+                <h2 className="text-xl">Create TITAN X Miner</h2>
+                <div>
+                    <div className="flex-between my-2">
+                        <span>Stake Amount</span>
+                        <span>
+                            <InputNumber min={1} max={10} defaultValue={3} onChange={() => {}} />
+                            <Button className="ml-2">MAX</Button>
+                        </span>
+                    </div>
+                    <div className="flex-between my-2">
+                        <span>Stake Length</span>
+                        <span>
+                            <InputNumber
+                                min={1}
+                                max={3500}
+                                defaultValue={3500}
+                                onChange={() => {}}
+                            />
+                            <Button className="ml-2">MAX</Button>
+                        </span>
+                    </div>
+                </div>
+                <Button block className="my-4">
+                    Start Stake
+                </Button>
+                <p className="text-gray-500 text-sm text-center my-2">
+                    don't have TITAN X? buy here or mine here.
+                </p>
+            </div>
+        );
+    };
+
+    const infoData = [
+        {
+            key: "1",
+            label: "Stake Summary",
+            content: [
+                {
+                    key: "1.1",
+                    label: "TITAN X in Stake",
+                    value: "0",
+                    tips: "TITAN X in Stake",
+                },
+                {
+                    key: "1.2",
+                    label: "# of Shares",
+                    value: "3",
+                    tips: "# of Shares",
+                },
+                {
+                    key: "1.3",
+                    label: "$ Market Value of Miner",
+                    value: "3",
+                    tips: "Market Value of Miner",
+                },
+                {
+                    key: "1.4",
+                    label: "Est. ROI % at End of Miner",
+                    value: "3",
+                    tips: "Est. ROI % at End of Miner",
+                },
+            ],
+        },
+        {
+            key: "2",
+            label: "TITAN X Stake Details (Estimations)",
+            content: [
+                {
+                    key: "2.1",
+                    label: "Current Share Rate (excl. Bonuses)",
+                    value: "828.57",
+                    tips: "Current Share Rate (excl. Bonuses)",
+                },
+                {
+                    key: "2.2",
+                    label: "Base Shares (excl. Bonuses)",
+                    value: "+0",
+                    tips: "Base Shares (excl. Bonuses)",
+                },
+                {
+                    key: "2.3",
+                    label: "Stake Share Bonuses",
+                    value: "+0",
+                    tips: "Stake Share Bonuses",
+                },
+                {
+                    key: "2.4",
+                    label: "Effective Share Rate (incl. Bonuses)",
+                    value: "+0",
+                    tips: "Effective Share Rate (incl. Bonuses)",
+                },
+                {
+                    key: "2.1",
+                    label: "Effective Shares (incl. Bonuses)",
+                    value: "0",
+                    tips: "Effective Shares (incl. Bonuses)",
+                },
+            ],
+        },
+    ];
+
+    return (
+        <div>
+            <ContentWrapper
+                title="Stake"
+                subTitle="Earn ETH Passive Income by staking your TITAN X"
+            >
+                <div className="flex gap-4">
+                    <div className="w-1/2">
+                        <SingleMiner />
+                    </div>
+                    <div className="w-1/2">
+                        <InfoCard data={infoData} />
+                    </div>
+                </div>
+            </ContentWrapper>
+        </div>
+    );
 }
 
 export default index;
