3865377eb89c38bad9d45f823bda55926e2871ce
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-03-06 17:35:41 +0800

    Feat: button css

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -10,6 +10,7 @@ import NotFound from "@/pages/NotFound";
 import BuyAndBurn from "@/pages/BuyAndBurn";
 import Stats from "@/pages/Stats";
 import Calculator from "@/pages/Calculator";
+import Component from "@/pages/Component";
 
 function App() {
     return (
@@ -22,6 +23,7 @@ function App() {
                     <Route path="buyandburn" element={<BuyAndBurn />} />
                     <Route path="stats" element={<Stats />} />
                     <Route path="calculator" element={<Calculator />} />
+                    <Route path="component" element={<Component />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </HomeLayout>
---
+++ FrontEnd/src/assets/index.ts
@@ -0,0 +1,1 @@
+export { default as BgCard1 } from "@/assets/svgs/BgCard1.svg";
